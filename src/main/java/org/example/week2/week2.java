2545e203da71709aa8e3308108f40513e3a2f24e
Timur <timur.puzo@stu.ibu.edu.ba>
2024-10-13 19:49:22 +0200

    Week 2

---
+++ src/main/java/org/example/week2/week2.java
@@ -0,0 +1,125 @@
+package org.example.week2;
+import java.util.Scanner;
+public class week2 {
+    /*
+    public static void main(String[] args) {
+
+        /*
+        //task1
+        Scanner unesi = new Scanner(System.in);
+        System.out.println("Unesi sifru: ");
+        String sifru = unesi.nextLine();
+
+        while(!sifru.equals("carrot")) {
+            System.out.println("Pogresno, unesi sifru: ");
+            sifru = unesi.nextLine();
+        }
+        System.out.println("Svaka cast");
+    }
+         */
+        /*
+        //task2
+        Scanner broj1 = new Scanner(System.in);
+        Scanner broj2 = new Scanner(System.in);
+        Scanner broj3 = new Scanner(System.in);
+
+        int num1 = Integer.parseInt(broj1.nextLine());
+        int num2 = Integer.parseInt(broj2.nextLine());
+        int num3 = Integer.parseInt(broj3.nextLine());
+
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+         */
+        /*
+        //task3
+        Scanner broj1 = new Scanner(System.in);
+        Scanner broj2 = new Scanner(System.in);
+        Scanner broj3 = new Scanner(System.in);
+
+        int num1 = Integer.parseInt(broj1.nextLine());
+        int num2 = Integer.parseInt(broj2.nextLine());
+        int num3 = Integer.parseInt(broj3.nextLine());
+        int sum = num1 + num2 + num3;
+        if(num1 == 0 || num2 == 0 || num3 == 0){
+            System.out.println("Fula");
+        }else{
+            System.out.println(sum);
+        }
+        //int sum = num1 + num2 + num3;
+        //System.out.println(sum);
+         */
+        /*
+        //task4
+        int pocetni = 1;
+        int kraj = 9;
+
+        while(pocetni <= kraj) {
+            pocetni++;
+            if(pocetni == kraj) {
+                break;
+            }
+            System.out.println(pocetni);
+        }
+         */
+        /*
+        //task5
+        Scanner num1 = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        int b = Integer.parseInt(num1.nextLine());
+        int a = 2;
+        System.out.println((int)Math.pow(a,b));
+         */
+        /*
+        //task6
+        String text = "In the beginning there were the swamp, the hoe and Java";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Unesi broj: ");
+        int num = Integer.parseInt(sc.nextLine());
+        for(int i = 1; i<=num; i++){
+            System.out.println("In the beginning there were the swamp, the hoe and Java" + "\n");
+        }
+         */
+        /*
+        //task7
+        public static void main(String[] args) {
+            funkcija(5);
+        }
+        public static void funkcija(int num){
+            for (int i = 1; i <= num; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("* " );
+                }
+                System.out.println();
+            }
+        }
+         */
+    /*
+    //task8
+    public static void main(String[] args) {
+        funkcija(6);
+    }
+    public static void funkcija(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+     */
+    /*
+    //task9
+    public static void main(String[] args) {
+        funkcija(5);
+    }
+    public static void funkcija(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+     */
+}
+
